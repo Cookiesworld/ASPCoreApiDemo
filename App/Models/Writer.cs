bbd883b47611607e31071f0f6c73e801e47d33de
John.Cooke <John.Cooke@christie.nhs.uk>
2019-10-14 11:51:57 +0100

    Added

---
+++ App/Models/Writer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Authors.Models
+{
+    public class Writer
+    {
+        public Writer()
+        {
+        }
+
+        public Writer(string name, DateTime? dateOfBirth)
+        {
+            this.Name = name;
+            this.DateOfBirth = dateOfBirth;
+        }
+
+        [Key]
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
